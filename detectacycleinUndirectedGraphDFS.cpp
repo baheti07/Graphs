c253a2b1a126f9bf7a9184fd30df88bdd747e94d
baheti07 <90107385+baheti07@users.noreply.github.com>
2023-06-22 17:53:28 +0530

    Add files via upload

---
+++ detectacycleinUndirectedGraphDFS.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+bool checkForCycle(int node, int parent, vector < int > & vis, vector < int > adj[]) {
+      vis[node] = 1;
+      for (auto it: adj[node]) {
+        if (!vis[it]) {
+          if (checkForCycle(it, node, vis, adj))
+            return true;
+        } else if (it != parent)
+          return true;
+      }
+
+      return false;
+    }
+  
+    bool isCycle(int V, vector < int > adj[]) {
+      vector < int > vis(V + 1, 0);
+      for (int i = 0; i < V; i++) {
+        if (!vis[i]) {
+          if (checkForCycle(i, -1, vis, adj)) return true;
+        }
+      }
+
+      return false;
+    }
+    void addEdge(vector<int> adj[],int u,int v)
+{
+    adj[u].push_back(v);
+    adj[v].push_back(u);
+}
+int main()
+{
+    vector<int> adj[5];
+   
+    addEdge(adj,0,1);
+    addEdge(adj,0,2);
+    addEdge(adj,2,3);
+    addEdge(adj,1,3);
+    addEdge(adj,2,4);
+   
+
+    int num=isCycle(5, adj);
+    if(num==1)
+      cout<<"Yes"<<endl;
+    else
+       cout<<"No"<<endl;
+ 
+    return 0;
+}
