d419272cd27c6778ce9efc234ef0dc535986c67b
baheti07 <90107385+baheti07@users.noreply.github.com>
2023-06-26 18:42:24 +0530

    Add files via upload

---
+++ bipartiteGraphDfs.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool dfs(int node, int col, int color[], vector<int> adj[]) {
+        color[node] = col; 
+        
+        // traverse adjacent nodes
+        for(auto it : adj[node]) {
+            // if uncoloured
+            if(color[it] == -1) {
+                if(dfs(it, !col, color, adj) == false) return false; 
+            }
+            // if previously coloured and have the same colour
+            else if(color[it] == col) {
+                return false; 
+            }
+        }
+        
+        return true; 
+    }
+
+	bool isBipartite(int V, vector<int>adj[]){
+	    int color[V];
+	    for(int i = 0;i<V;i++) color[i] = -1; 
+	    
+	    // for connected components
+	    for(int i = 0;i<V;i++) {
+	        if(color[i] == -1) {
+	            if(dfs(i, 0, color, adj) == false) 
+	                return false; 
+	        }
+	    }
+	    return true; 
+	}
+
+void addedge(vector<int> adj[],int u,int v)
+{
+        adj[u].push_back(v); 
+        adj[v].push_back(u);
+}
+int main() {
+    
+    vector<int> adj[6];
+    addedge(adj,0,1);
+    addedge(adj,1,2);
+    addedge(adj,1,4);
+    addedge(adj,1,5);
+    addedge(adj,2,3);
+    addedge(adj,3,4);
+    addedge(adj,3,5);
+
+    if(checkBipartite(adj, 6)) {
+        cout << "It is a Bipartite Graph"; 
+    } else {
+        cout << "It is not a Bipartite Graph"; 
+    }
+    return 0;
+}
